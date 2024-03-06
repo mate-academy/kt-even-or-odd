201ce3df3096c2f483ad118ae7641d02381b16f1
Сидоркін Андрій Володимирович <sydorkin-av@ukrposhta.ua>
2024-03-06 22:19:59 +0200

    task is implemented and tested

--- src/main/kotlin/mate/academy/EvenOrOdd.kt
+++ src/main/kotlin/mate/academy/EvenOrOdd.kt
@@ -2,3 +2,11 @@ package mate.academy
 
 // Create a function `evenOrOdd` that takes an integer as an argument
 // and returns "Even" for even numbers or "Odd" for odd numbers.
+
+fun evenOrOdd(number: Int) = when {
+    number % TWO == ZERO -> "Even"
+    else -> "Odd"
+}
+
+const val ZERO = 0
+const val TWO = 2
