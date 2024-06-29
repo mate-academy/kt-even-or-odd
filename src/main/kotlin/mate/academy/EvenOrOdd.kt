0975631dcb57f1c24057b0e1825d2d9d84e077cb
igortrohimcuk <trokhymchuk.ihor.dev@gmail.com>
2024-06-29 16:09:16 +0300

    Solution

--- src/main/kotlin/mate/academy/EvenOrOdd.kt
+++ src/main/kotlin/mate/academy/EvenOrOdd.kt
@@ -1,4 +1,6 @@
 package mate.academy
 
-// Create a function `evenOrOdd` that takes an integer as an argument
-// and returns "Even" for even numbers or "Odd" for odd numbers.
+private const val EVEN = "Even"
+private const val ODD = "Odd"
+
+fun evenOrOdd(number: Int): String = if(number % 2 == 0) EVEN else ODD
