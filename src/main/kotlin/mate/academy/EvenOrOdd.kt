63a5bda1f9b397f25973407bc04dc4dc4102e87f
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-03-14 15:31:01 +0200

    Implemented evenOrOdd function to determine number parity.

--- src/main/kotlin/mate/academy/EvenOrOdd.kt
+++ src/main/kotlin/mate/academy/EvenOrOdd.kt
@@ -1,4 +1,6 @@
 package mate.academy
 
-// Create a function `evenOrOdd` that takes an integer as an argument
-// and returns "Even" for even numbers or "Odd" for odd numbers.
+const val EVEN_NUMBER = "Even"
+const val ODD_NUMBER = "Odd"
+
+fun evenOrOdd(number: Int): String = if(number % 2 == 0) EVEN_NUMBER else ODD_NUMBER
